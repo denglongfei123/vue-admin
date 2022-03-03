100652fc13c33d3cfc51e6141f3b330034a4d8d4
denglongfei123 <511103794@qq.com>
2022-03-03 23:41:33 +0800

    vue.config配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+const path = require('path')
+const name = 'vue admin'
+
+function resolve(dir){
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath:'/',
+  outputDir:'dist',
+  assetsDir:'static',
+  configureWebpack:{
+    name: name,
+    resolve:{
+      alias:{
+        '@':resolve('src')
+      }
+    }
+  }
+}
